f8969c8af93a20739db10458630a5d2bc3fc8662
Hồ Thái Khanh <hothaikhanh@gmail.com>
2023-07-03 09:54:47 +0700

    ex_11, ex_12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldestPersonAge = 0;
+    let result = people.reduce((oldestPerson, currPerson) => {
+        if (currPerson.yearOfDeath == null) currPerson.yearOfDeath = 2023;
+        let currPersonAge = currPerson.yearOfDeath - currPerson.yearOfBirth;
 
+        if (currPersonAge > oldestPersonAge) {
+            oldestPersonAge = currPersonAge;
+            oldestPerson = currPerson;
+        }
+        return oldestPerson;
+    });
+
+    return result;
 };
 
 // Do not edit below this line
